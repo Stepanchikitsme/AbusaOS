f4f53ffe1edb35e78981544fef3c1ffc3ca95d24
stpanchikitsme <stpanchikitsi@gmail.com>
2024-05-26 15:12:29 +0700

    New explorer icons

--- AbusaOS/obj/Debug/net6.0/AbusaOS.AssemblyInfo.cs
+++ AbusaOS/obj/Debug/net6.0/AbusaOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Use VMware Player or Workstation to deploy and debug.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b94e99da38cb74110399fe2705b10c12e8abd8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd31d9ec6cf6bbfee7380c22c3ad0aec11bd956c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbusaOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbusaOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
